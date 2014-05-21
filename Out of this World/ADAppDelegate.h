a5d65200ee8770d5bd7f4267c9c806d7bf89dcc2
arturododd <arturo@dodd.mx>
2014-05-20 20:11:38 -0500

    Initial Commit

---
+++ Out of this World/ADAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADAppDelegate.h
+//  Out of this World
+//
+//  Created by Arturo Dodd Vaudrecourt on 5/20/14.
+//  Copyright (c) 2014 Dodd.mx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/ADViewController.m
@@ -0,0 +1,29 @@
+//
+//  ADViewController.m
+//  Out of this World
+//
+//  Created by Arturo Dodd Vaudrecourt on 5/20/14.
+//  Copyright (c) 2014 Dodd.mx. All rights reserved.
+//
+
+#import "ADViewController.h"
+
+@interface ADViewController ()
+
+@end
+
+@implementation ADViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
